c66bb58547e2c2c4c39d3e383d7d9081ea3e679b
sarthu210 <sarthu102@gmail.com>
2024-08-17 21:47:18 +0530

    Sign-In Code Completed!

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -2,6 +2,24 @@ import { StudModel } from "../models/student.model";
 import mongoose from "mongoose";
 import bcrypt from "bcrypt"
 
+
+async function generateAccessTokenAndRefreshToken(userId){
+    try {
+        const user = await StudModel.findById(userId);
+        const accesToken = user.generateAccessToken();
+        const refreshToken = user.generateRefreshToken();
+    
+        user.refreshToken = refreshToken;
+    
+        await user.save({ validateBeforeSave: false});
+    
+        return {accesToken, refreshToken};
+
+    } catch (error) {
+        console.log("Error Occured While Generating Access And Refresh Token", error);
+    }
+}
+
 async function SignUp(req, res, next) {
     try {
         const {EnNumber, email, password} = req.body;
@@ -45,6 +63,47 @@ async function SignUp(req, res, next) {
             message: "Error Occured while sign-up"
         })
     }
+}
 
+async function LogIn(req,res,next) {
+    try {
+        const { EnNumber, password } = req.body;
 
+        const checkEnNumber = await StudModel.findOne(EnNumber);
+
+        if(!checkEnNumber)
+        {
+            return res.status(400).json({
+                message: "User Not Found!"
+            })
+        }
+
+        const user = await StudModel.findOne(EnNumber);
+
+        const checkPassowrd = await StudModel.isPasswordCorrect(user.password);
+
+        if(!checkPassowrd)
+        {
+            return res.status(400).json({
+                message: "Incorrect Password"
+            })
+        }
+
+        const {accesToken , refreshToken} = await generateAccessTokenAndRefreshToken(user._id);
+
+        const loggedUserId = await StudModel.findById(user._id).select("-password -refreshToken");
+
+        const options = {
+            httpOnly: true,
+            secure: true
+        }
+
+        return res.status(200).cookie("accessToken",accesToken,options).cookie("refreshToken",refreshToken,options).json(loggedUserId,user);
+    
+    } catch (error) {
+        console.log("Error Occurd While Sign-In");
+        return res.status(400).send({
+            message: "Error Occurd While Sign-In"
+        })
+    }
 }
